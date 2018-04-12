72f86533789a075db28c418787f858e7606c6fbd
kevinzhao4939 <34120109+kevinzhao4939@users.noreply.github.com>
2018-04-11 23:36:20 -0400

    Add files via upload

---
+++ MazeSolver.java
@@ -0,0 +1,14 @@
+/**
+
+  Represent a maze, containing an existing treasure for use with the maze
+  solver problem.
+
+  A space " " respresents a wall, a "x" represents a stepping stone, where
+  the explorer is legally allowed to move onto, an "e" is the explorer, and
+  a "0" is the desired treasure.
+
+  MazeSolver.java will find a path between the explorer and the treasure,
+  given that there is a *legal* travelling path and that there is an
+  existing treasure.
+
+*/
---
+++ UserOfMazeSolver.java
@@ -0,0 +1,8 @@
+/**
+
+  Test MazeSolver.java
+
+  Specify where the explorer is, the makeup of the maze, and if there is
+  or isn't a legal travelling path, and where the treasure is located.
+
+*/
