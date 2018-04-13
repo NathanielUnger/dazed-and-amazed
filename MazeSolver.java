3bee58763e3cf9f0a56cb6bca6ca77e1dda50590
kevinzhao4939 <34120109+kevinzhao4939@users.noreply.github.com>
2018-04-12 22:05:23 -0400

    Add files via upload

--- MazeSolver.java
+++ MazeSolver.java
@@ -1,14 +1,25 @@
 /**
-
   Represent a maze, containing an existing treasure for use with the maze
   solver problem.
-
   A space " " respresents a wall, a "x" represents a stepping stone, where
   the explorer is legally allowed to move onto, an "e" is the explorer, and
   a "0" is the desired treasure.
-
   MazeSolver.java will find a path between the explorer and the treasure,
   given that there is a *legal* travelling path and that there is an
   existing treasure.
-
 */
+
+public class MazeSolver {
+  private static int[] directions = new int[]{Maze.NORTH, Maze.EAST, Maze.SOUTH, Maze.WEST};
+  public static boolean solve(Maze maze) {
+    Maze snapshot = new Maze(maze);
+    if (maze.explorerIsOnA() == 0) {
+			return true;
+    }
+    else {
+      maze.go(Maze.EAST);
+      System.out.println(maze);
+      solve(maze);
+    }
+    return false;
+  }}
--- UserOfMazeSolver.java
+++ UserOfMazeSolver.java
@@ -1,8 +1,21 @@
 /**
-
   Test MazeSolver.java
-
   Specify where the explorer is, the makeup of the maze, and if there is
   or isn't a legal travelling path, and where the treasure is located.
-
 */
+
+public class UserOfMazeSolver {
+    private static Displayer displayer;
+
+    public static void main(String[] commandLine)
+       throws java.io.FileNotFoundException {
+        System.out.println();
+
+        Maze maze = new Maze( commandLine[0]
+                            , Integer.parseInt( commandLine[1])
+                            , Integer.parseInt( commandLine[2])
+                            );
+        System.out.println( maze + System.lineSeparator());
+
+        System.out.println(MazeSolver.solve(maze));
+    }}
